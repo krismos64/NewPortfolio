edc3183933f1698de2335cfa9e4b8bc3d44f1689
krismos64 <134715062+krismos64@users.noreply.github.com>
2024-07-18 17:16:50 +0200

    updtae projects

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -7,50 +7,46 @@ use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Swift_Mailer;
 
 class ContactController extends AbstractController
-
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(Request $request, \Swift_Mailer $mailer, UserRepository $userRepository)
+    public function index(Request $request, Swift_Mailer $mailer, UserRepository $userRepository)
     {
-        $users    = $userRepository->findAll(); 
+        $users = $userRepository->findAll(); 
         $form = $this->createForm(ContactType::class);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid())
-        {
+        if ($form->isSubmitted() && $form->isValid()) {
             $contact = $form->getData();
 
-            //Ici on envoi le mail 
+            // Ici on envoie le mail 
             $message = (new \Swift_Message('Nouveau Contact'))
-                // On attribue l'expediteur du $message
+                // On attribue l'expéditeur du message
                 ->setFrom($contact['email'])
-
                 // On attribue le destinataire
-                ->setTo('sangodim@gmail.com')
-
+                ->setTo('c.mostefaoui@yahoo.fr')
                 // On crée le message avec la vue twig
                 ->setBody(
                     $this->renderView(
                         'email/contact.html.twig', compact('contact')
                     ),
                     'text/html'
-                )
-            ;
+                );
 
-            // On envoie le $message
+            // On envoie le message
             $mailer->send($message);
-            $this->addFlash('success', 'Votre message a été transmis, nous vous répondrons dans les meilleurs délais.');
+            $this->addFlash('success', 'Votre message a été transmis, je vous répondrai dans les meilleurs délais.');
+
             return $this->redirectToRoute('home');
         }
 
-
         return $this->render('contact/contact.html.twig', [
-            'form'     => $form->createView(),
-            'users'    => $users 
+            'form' => $form->createView(),
+            'users' => $users 
         ]);
     }
 }
